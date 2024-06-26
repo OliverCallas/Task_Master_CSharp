1898726443dfff1582d5ae9999511fbeced292c2
OliverCallas <A18101119@idat.edu.pe>
2024-06-26 17:53:03 -0500

    Commit 1.1: Main panel basic implementation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+namespace TaskMaster
+{
+    class Program
+    {
+
+        static readonly string[] Options = ["1. See tasks", "2. Manage Task", "3. Consult tasks by:", "4. Mark task completed", "5. Salir"];
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                foreach (string option in Options)
+                {
+                    Console.WriteLine(option);
+                }
+
+                exit = true;
+            }
+        }
+    }
+}
